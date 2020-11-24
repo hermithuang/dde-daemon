d2929477037973504bc7f8cb1a4a1278353848b1
wubowen <wubowen@uniontech.com>
2020-11-24 11:11:46 +0800

    fix(display): Clear tty print data
    使用root权限清除tty的数据
    
    Log: FROM是:0/:1等类型的tty需要使用root权限删除数据，-类型的tty可以直接删除print数据
    Bug: https://pms.uniontech.com/zentao/bug-view-52120.html
    Bug: https://pms.uniontech.com/zentao/bug-view-54048.html
    Change-Id: I8316b886189fd1e30ff38333abaed2f432a0cddb

--- bin/dde-system-daemon/main.go
+++ bin/dde-system-daemon/main.go
@@ -56,6 +56,7 @@ type Daemon struct {
 		NetworkGetConnections          func() `out:"data"`
 		NetworkSetConnections          func() `in:"data"`
 		BluetoothGetDeviceTechnologies func() `in:"adapter,device" out:"technologies"`
+		ClearTtys	       			   func()
 	}
 	signals *struct {
 		HandleForSleep struct {
